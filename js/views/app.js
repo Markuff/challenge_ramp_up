695ec2cedf063db49606ea301ab600ae41e88387
Sebastian Duarte <sebastian.duarte@digitaslbi.com>
2015-12-29 14:21:21 -0500

    Already pulling data, now working on render

--- js/views/app.js
+++ js/views/app.js
@@ -3,5 +3,13 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend ({
-    el:'#applic'
+    el:'#applic',
+    initialize: function () {
+        this.listenTo(app.Crimes, 'add', this.fill);
+        app.Crimes.fetch();
+    },
+    fill: function () {
+        var view = new app.CrimeView();
+        $('applic').append(view.render().el);
+    }
 });
