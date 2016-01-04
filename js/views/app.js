5fea14109aa58fe32733e293b416bec5094db5f3
Sebastian Duarte <sebastian.duarte@digitaslbi.com>
2016-01-04 09:43:57 -0500

    New week check, added commeted elements for future Handlebars

--- js/views/app.js
+++ js/views/app.js
@@ -12,7 +12,7 @@ app.AppView = Backbone.View.extend ({
         console.log(crimes);
         for(var i = 0; i < crimes.length; i++) {
             var view = new app.CrimeView({model:crimes.models[i]});
-            console.log(crimes.models[i]);
+            //console.log(crimes.models[i]);
             $('#content').append(view.render().el);
         }
     }
