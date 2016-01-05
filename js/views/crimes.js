d76f5b2ee914551c00b39d91c5aa8364f5730065
Sebastian Duarte <sebastian.duarte@digitaslbi.com>
2016-01-05 10:16:56 -0500

    working on the Handlebars adition

--- js/views/crimes.js
+++ js/views/crimes.js
@@ -5,7 +5,8 @@ var app = app || {};
 app.CrimeView = Backbone.View.extend ({
     tagName: 'div',
     className: 'each-crime',
-    template: _.template( $('#crimes').html() ),
+    //template: _.template( $('#crimes').html() ),
+    template: Handlebars.compile($('#crimes').html()),
     render: function () {
         this.$el.html( this.template( this.model.attributes ) );
         return this;
