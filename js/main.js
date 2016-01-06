525950c7a7dbb7da44dd583c89ba0efde334b9ab
Sebastian Duarte <sebastian.duarte@digitaslbi.com>
2016-01-06 11:10:59 -0500

    Libraries loading from main.js with the use of RequireJS, moving to make it with the rest of the files

--- js/main.js
+++ js/main.js
@@ -2,24 +2,25 @@
 
 // All other files named here, are relative in postion to this file.
 
-require.config({
+requirejs.config({
+    baseUrl: 'js',
     paths: {
-        'jQuery': 'lib/jquery-1.11.3.min',
-        'underscore': 'lib/underscore-min',
-        'backbone': 'lib/backbone-min',
-        'handlebars': 'lib/handlebars-v4.0.5',
+        jQuery: 'lib/jquery-1.11.3.min',
+        underscore: 'lib/underscore-min',
+        backbone: 'lib/backbone-min',
+        handlebars: 'lib/handlebars-v4.0.5',
+        crime_Model: 'models/crime',
     },
     shim: {
-        'jQuery': '$',
-        'underscore': '_',
-        'backbone': 'Bckb',
-        'handlebars': 'Hbs',
-    },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'crime_Model': {
+            exports: 'crime_model'
+        },
+    }
 });
 
-//Load made modules
-requirejs(['jQuery', 'underscore', 'backbone', 'handlebars'], function ($, _, Bckb, Hbs) {
-    //if ($) {console.log ('jQuery loaded...');}
-    //if (_) {console.log ('handlebars loaded...');}
-    //return true;
-});
+requirejs(['jQuery', 'underscore', 'backbone', 'handlebars', 'crime_Model'], 
+          function ($, _, Backbone, handlebars, crime_model) {});
