9a62bcd2337612a06a0fce50ba7b1ee27d319a4a
Sebastian Duarte <sebastian.duarte@digitaslbi.com>
2016-01-06 11:52:09 -0500

    check point

--- js/main.js
+++ js/main.js
@@ -9,18 +9,37 @@ requirejs.config({
         underscore: 'lib/underscore-min',
         backbone: 'lib/backbone-min',
         handlebars: 'lib/handlebars-v4.0.5',
-        crime_Model: 'models/crime',
+        crime_Mod: 'models/crime',
+        crimes_Col: 'collections/crimes',
+        crimes_Vie: 'views/crimes',
+        app_Vie: 'views/app',
+        app_JS: 'app'
     },
     shim: {
         'backbone': {
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
         },
-        'crime_Model': {
-            exports: 'crime_model'
+        'crime_Mod': {
+            exports: 'crime_mod'
         },
+        'crimes_Col': {
+            exports: 'crimes_col'
+        },
+        'crimes_Vie': {
+            exports: 'crimes_vie'
+        },
+        'app_Vie': {
+            exports: 'app_vie'
+        },
+        'app_JS': {
+            exports: 'app_js'
+        }
     }
 });
 
-requirejs(['jQuery', 'underscore', 'backbone', 'handlebars', 'crime_Model'], 
-          function ($, _, Backbone, handlebars, crime_model) {});
+requirejs(
+    ['jQuery', 'underscore', 'backbone', 'handlebars', 'crime_Mod', 'crimes_Col', 'crimes_Vie', 'app_Vie', 'app_JS'], 
+    function ($, _, Backbone, handlebars, crime_mod, crimes_col, crimes_vie, app_vie, app_js) 
+    {}
+);
--- js/views/crimes.js
+++ js/views/crimes.js
@@ -1,13 +1,18 @@
 // js/views/crimes.js
 
-var app = app || {};
 
-app.CrimeView = Backbone.View.extend ({
-    tagName: 'div',
-    className: 'each-crime',
-    template: Handlebars.compile($('#crimes').html()),
-    render: function () {
-        this.$el.html( this.template( this.model.attributes ) );
-        return this;
-    },
+requirejs(['handlebars'], function () {
+    
+    var app = app || {};
+
+    app.CrimeView = Backbone.View.extend ({
+        tagName: 'div',
+        className: 'each-crime',
+        template: Handlebars.compile($('#crimes').html()),
+        render: function () {
+            this.$el.html( this.template( this.model.attributes ) );
+            return this;
+        },
+    });
+    
 });
